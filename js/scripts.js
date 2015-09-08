e181bf13416c9fc4e3462d24666bb1a12dc99b5c
Jason Creighton & Kenny Grage <student@epicodus.com>
2015-09-08 10:44:12 -0700

    add functionality to Find Out! button

--- js/scripts.js
+++ js/scripts.js
@@ -16,3 +16,19 @@ function leapYear(year) {
     return false;
   }
 };
+
+$(document).ready(function() {
+  $("form#leap-year").submit(function(event) {
+    var year = parseInt($("input#year").val());
+    var result = leapYear(year);
+    $(".not").empty();
+
+    $(".year").text(year);
+    if (!result) {
+      $(".not").text("not");
+    }
+
+    $("#result").show();
+    event.preventDefault();
+  });
+});
