2417fd72ad9e625e84f8ad4da631490684a2595c
Jason Creighton & Kenny Grage <student@epicodus.com>
2015-09-08 10:10:17 -0700

    finish project

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+function leapYear(year) {
+  if (year % 4 === 0) {
+    if (year % 100 === 0) {
+      if (year % 400 === 0) {
+        return true;
+      }
+      else {
+        return false;
+      }
+    }
+    else {
+      return true;
+    }
+  }
+  else {
+    return false;
+  }
+};
